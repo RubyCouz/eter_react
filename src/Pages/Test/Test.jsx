5aa4ed7980a1347560fde9f7a495e5931dddb880
Jinan <jinan@pop-os.localdomain>
2020-10-29 17:53:36 +0100

    [Page] [Test] Modification pour tester le login

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -1,15 +1,33 @@
 import React from "react"
 import { Typography, Button, Grid } from "@material-ui/core"
-import { useHistory, useParams } from "react-router-dom"
+import { useHistory, NavLink } from "react-router-dom"
+import { AccountContext } from '../../Context/AccountContext'
 
 export default function Test() {
     let history = useHistory();
+
+
+
+    //Utilisation du tokken et utilisation
+
+    const {SetJWT, JWT } = React.useContext(AccountContext);
+
+    const deconnection = () => {
+        SetJWT(null)
+    }
+
+    const connection = () => {
+        SetJWT("blabla")
+    }
 
     function backHome() {
         history.push("/")
     }
 
 
+
+
+    let message = JWT ? "Connecter": "Deconnecter"
     return(
 
         <Grid
@@ -26,6 +44,24 @@ export default function Test() {
             >
                 Index
             </Button>
+            <Button 
+                variant="contained"
+                color="primary"
+                onClick={connection}
+            >
+                Connection
+            </Button>
+            <Button 
+                variant="contained"
+                color="primary"
+                onClick={deconnection}
+            >
+                Déconnection
+            </Button>
+            <Typography>Etat : {message} </Typography>
+            <Typography>Tokken : {JWT} </Typography>
+            
+
         </Grid>
     )
 }
