148f57474f23590cb39c257b137a13b77ceebc77
Jinan <jinan@pop-os.localdomain>
2020-10-29 17:54:12 +0100

    [Page] [Error404] AJout de la page

---
+++ src/Pages/Error404/Error404.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Typography, Grid } from "@material-ui/core"
+
+export default function Test() {
+    return(
+        <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        >
+            <Typography>ERREUR 404</Typography>
+        </Grid>
+    )
+}
