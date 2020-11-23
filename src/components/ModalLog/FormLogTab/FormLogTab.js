841d24b666b36778a06172f514456f4d2e8252a6
Jat15 <jinan15@hotmail.com>
2020-11-23 14:34:28 +0100

    [Components] [ModalLog] [Form Log] Mise en forme

--- src/components/ModalLog/FormLogTab/FormLogTab.js
+++ src/components/ModalLog/FormLogTab/FormLogTab.js
@@ -1,12 +1,21 @@
 import React from 'react';
+
+import {
+    makeStyles,
+    useTheme,
+} from '@material-ui/core/styles';
+
+import {
+    Box,
+    Tab,
+    Tabs,
+    AppBar,
+    Typography,
+} from '@material-ui/core';
+
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
-import {makeStyles, useTheme} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+
 import LogInForm from "./LogInForm/LogInForm";
 import Signin from "./SignInForm/SignInForm";
 
