f9ca8a613761dac14ac627edb82688dc76398ea1
Jat15 <jinan15@hotmail.com>
2020-11-23 14:44:16 +0100

    [Pages] [Navbar] Mise en forme et ajout des imports  pour les cookie

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from "react"
-import {NavLink, useLocation} from "react-router-dom"
 import clsx from 'clsx';
-import {makeStyles, useTheme} from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-import RowingOutlinedIcon from '@material-ui/icons/RowingOutlined';
-import Button from "@material-ui/core/Button"
-import {Box, Grid} from "@material-ui/core";
+
+import {
+    NavLink,
+    useLocation,
+} from "react-router-dom"
+
+import {
+    makeStyles,
+    useTheme,
+} from '@material-ui/core/styles';
+
+import {
+    Menu as MenuIcon,
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon,
+    HomeOutlined as HomeOutlinedIcon,
+    RowingOutlined as RowingOutlinedIcon,
+} from '@material-ui/icons';
+
+import {
+    Box,
+    Grid,
+    Button,
+    CssBaseline,
+    Typography,
+    Drawer,
+    AppBar,
+    Toolbar,
+    List,
+    Divider,
+    IconButton,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from "@material-ui/core";
+
+import { useCookies } from 'react-cookie';
+import { getData } from '../../Tools/Cookie/ManagingCookie'
 
 const drawerWidth = 240;
 
