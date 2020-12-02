e8882febd1396453e668e83bf35ba46d94a03564
Jat15 <jinan15@hotmail.com>
2020-12-02 14:20:57 +0100

    [Routes] [Private Route} Mise a jour
    - Posibilite de choisir un roles pour la route privée

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,33 +1,43 @@
-import React, { useContext } from 'react';
+import React, {
+  useContext,
+} from 'react';
 
 import {
   Route,
   Redirect,
 } from 'react-router-dom';
 
-import { AccountContext } from '../Context/AccountContext';
+import {
+  AccountContext
+} from '../Context/AccountContext';
 
 export default function PrivateRoute({ children, ...rest }) {
 
   const { sessionData } = useContext( AccountContext );
 
-
-  const userAdmin = sessionData['login'] ? 
-    sessionData['roles'][0] === 'ROLE_ADMIN'?
-      true
-      : false
-  : false
+  const userRole =
+    sessionData['login'] ? 
+      rest.role ?
+        sessionData['roles'][0] === rest.role
+      : 
+        true
+      ?
+        true
+      : 
+        false
+    :
+      false
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        userAdmin ? (
+        userRole ? (
           children
         ) : (
           <Redirect
             to={{
-              pathname: '/login',
+              pathname: '/home',
               state: { from: location }
             }}
           />
