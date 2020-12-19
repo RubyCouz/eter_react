d1f25f8321e8776396ad0f52da9f025866a03b4f
Jat15 <jinan15@hotmail.com>
2020-12-19 15:07:21 +0100

    [Pages] [BacOffice] Refonte
    - Utilisation de la pagination
    - Possibilité de trier
    - Possibilité de sélectionner

--- src/Pages/BackOffice/BackOffice.js
+++ src/Pages/BackOffice/BackOffice.js
@@ -5,153 +5,372 @@ import
         useContext,
         useMemo,
 } from 'react'
+import clsx from 'clsx'
+import { lighten, makeStyles } from '@material-ui/core/styles'
+
 
 import {
-    TableContainer,
-    Table,
+    TablePagination,
     TableHead,
-    TableBody,
-    TableRow,
+    TableContainer,
     TableCell,
-} from '@material-ui/core'
+    TableBody,
+    Table,
+    TableRow,
+    TableSortLabel,
+    Toolbar,
+    Typography,
+    Paper,
+    Checkbox,
+    IconButton,
+    Tooltip,
+    FormControlLabel,
+    Switch,
+} from '@material-ui/core';
 
-import Request from '../../Tools/Request/Request'
+import {
+    Delete as DeleteIcon,
+    FilterList as FilterListIcon,
+} from '@material-ui/icons';
 
 import { AccountContext } from '../../Context/AccountContext';
-
-export default function BackOffice() {
-
-    const { sessionData } = useContext( AccountContext );
-    const xsrf =  sessionData['login'] ? sessionData["xsrf-token"] : false
-
-    const [table, setTable] = useState()
+import Request from '../../Tools/Request/Request'
 
 
-    // Template pour les informations
-    // 0 est prit comme key des lignes
-    const [templateData] = useState({
-        0 : {
-            nameColumn: "Utilisateur",
-            nameQuery: "userLogin",
-        },
-        1 : {
-            nameColumn: "Role",
-            nameQuery: "userRole",
-        },
-        3 : {
-            nameColumn: "Date",
-            nameQuery: "userDate",
-        },
-    })
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-    //Création de la requête
-    const query = useMemo(
-        () => {
-            let queryColumn = "" ;
-            for ( const [ key ] of Object.entries( templateData ) ) {
-                queryColumn += " " + templateData[ key ][ "nameQuery" ]
-            }
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
 
-            const query = {
-                query : `
-                    query {
-                        eterUsers ( first: 100 ) {
-                            edges {
-                                node {
-                                    ${queryColumn}
-                                }
-                            }
-                        }
-                    }
-                `
-            }
-            return query
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el.node, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  { id: 'userLogin', numeric: false, disablePadding: true, label: 'Utilisateur' },
+  { id: 'userRole', numeric: true, disablePadding: false, label: 'Role' },
+  { id: 'userDate', numeric: true, disablePadding: false, label: 'Date' },
+];
+
+
+
+function EnhancedTableHead(props) {
+  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ 'aria-label': 'select all desserts' }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
         }
-        , [templateData]
-    );
-    
-    //Création de l'en-tête du tableau
-    const header = useMemo(
-        () => {
-            let headerData = [];
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: '1 1 100%',
+  },
+}));
 
-            for ( const [ key ] of Object.entries( templateData ) ) {
-                headerData.push(
-                    <TableCell>
-                        { templateData[ key ][ "nameColumn" ] }
-                    </TableCell>
-                )
-            }
-            return headerData
-        }
-        , [templateData]
-    );
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
 
-    useEffect( () => {
-        function fetchMap(result) {
-            let table = [];
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+          Nutrition
+        </Typography>
+      )}
 
-            if ( !result.errors && result.data ) {
-               table = result.data.eterUsers.edges.map(val => {
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton aria-label="filter list">
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
 
-                    // création des cellules de la lignes
-                    let row = []
-                    for ( const [ key ] of Object.entries( templateData ) ) {
-                        row.push(
-                            <TableCell>
-                                { val.node[ templateData[ key ][ "nameQuery" ] ] }
-                            </TableCell>
-                        )
-                    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
 
-                    // création de la lignes du tableau
-                    
-                        return <TableRow
-                            key={ val.node[ templateData[ "0" ][ "nameQuery" ] ] }
-                        >
-                         { row }
-                        </TableRow>
-                    
-                })
-            } else {
-                table.push(
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+  const { sessionData } = useContext( AccountContext );
+  const xsrf =  sessionData['login'] ? sessionData["xsrf-token"] : false
+
+
+  //Création de la requête
+  const query = useMemo(
+      () => {
+          let queryColumn = "" ;
+
+          headCells.map((n) => queryColumn += " " + n.id )
+
+          const query = {
+              query : `
+                  query {
+                      eterUsers ( first: 100 ) {
+                          edges {
+                              node {
+                                  ${queryColumn}
+                              }
+                          }
+                      }
+                  }
+              `
+          }
+          return query
+      }
+      , []
+  )
+
+  const [rows, setRows] = useState([])
+
+  useEffect( () => {
+      Request( query, xsrf )
+      .then(function(result) {
+        setRows(Object.values(result.data.eterUsers.edges))
+        console.log(Object.values(result.data.eterUsers.edges))
+      })
+
+  },[])
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.node.userLogin);
+      console.log(rows)
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.userLogin);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
                     <TableRow
-                        key={"rien"}
+                      hover
+                      onClick={(event) => handleClick(event, row.userLogin)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.userLogin}
+                      selected={isItemSelected}
                     >
-                        <TableCell
-                            colSpan={2}
-                            align="center"
-                        >
-                            Rien !
-                        </TableCell>
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          checked={isItemSelected}
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </TableCell>
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.userLogin}
+                      </TableCell>
+                      <TableCell align="right">{row.userRole}</TableCell>
+                      <TableCell align="right">{row.userDate}</TableCell>
                     </TableRow>
-                )
-            }
-
-            setTable(table);
-        }
-
-        Request( query, xsrf )
-        .then(function(result) {
-            fetchMap(result);
-        })
-
-    },[])
-
-    return (
-        <div>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            { header }
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        { table }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    )
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </div>
+  );
 }
