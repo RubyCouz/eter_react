30b36cde894058ad83d6a1f5a64e30b46da40b60
Jat15 <jinan15@hotmail.com>
2020-12-24 16:13:13 +0100

    [Pages] [BackOffice] Modification et découpage
    - Supression la densité des colonnes
    - Découpage de la création de la toobar
    - Utilisation l'id pour identifier les ligne

--- src/Pages/BackOffice/BackOffice.jsx
+++ src/Pages/BackOffice/BackOffice.jsx
@@ -5,8 +5,7 @@ import
         useContext,
         useMemo,
 } from 'react'
-import clsx from 'clsx'
-import { lighten, makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import {
     TablePagination,
     TableHead,
@@ -16,48 +15,26 @@ import {
     Table,
     TableRow,
     TableSortLabel,
-    Toolbar,
+
     Typography,
     Paper,
     Checkbox,
-    IconButton,
-    Tooltip,
-    FormControlLabel,
-    Switch,
 } from '@material-ui/core';
-
-import {
-    Delete as DeleteIcon,
-    FilterList as FilterListIcon,
-} from '@material-ui/icons';
 
 import Request from '../../Tools/Request/Request'
 import BackOfficeTableRow from './BackOfficeTableRow'
+import EnhancedTableToolbar from './EnhancedTableToolbar'
 
 import { AccountContext } from '../../Context/AccountContext';
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
+import {getComparator} from '../../Tools/Filtre/Filtre'
 
 function stableSort(array, comparator) {
   const stabilizedThis = array.map((el, index) => [el.node, index]);
   stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
   });
   return stabilizedThis.map((el) => el[0]);
 }
@@ -82,10 +59,10 @@ function EnhancedTableHead(props) {
       <TableRow>
         <TableCell padding="checkbox">
           <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
+            indeterminate = { numSelected > 0 && numSelected < rowCount }
+            checked = { rowCount > 0 && numSelected === rowCount }
+            onChange = { onSelectAllClick }
+            inputProps = {{ 'aria-label': 'select all desserts' }}
           />
         </TableCell>
         <TableCell padding="checkbox" >
@@ -117,64 +94,6 @@ function EnhancedTableHead(props) {
   );
 }
 
-//Style de la toolbar
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
-
-//La toolbar
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-          Les membres
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list">
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -185,7 +104,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
   },
   table: {
-    minWidth: 750,
+    minWidth: 300,
   },
   visuallyHidden: {
     border: 0,
@@ -207,7 +126,7 @@ export default function EnhancedTable() {
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
+
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
 
@@ -218,24 +137,25 @@ export default function EnhancedTable() {
   //Création de la requête
   const query = useMemo(
       () => {
-          let queryColumn = "" ;
 
-          headCells.map((n) => queryColumn += " " + n.id )
+        let queryColumn = "" ;
+        headCells.map((n) => queryColumn += " " + n.id )
 
-          const query = {
-              query : `
-                  query {
-                      eterUsers ( first: 100 ) {
-                          edges {
-                              node {
-                                  ${queryColumn}
-                              }
-                          }
-                      }
-                  }
-              `
-          }
-          return query
+        const query = {
+            query : `
+                query {
+                    eterUsers ( first: 100 ) {
+                        edges {
+                            node {
+                                id
+                                ${queryColumn}
+                            }
+                        }
+                    }
+                }
+            `
+        }
+        return query
       }
       , []
   )
@@ -247,10 +167,8 @@ export default function EnhancedTable() {
       Request( query, xsrf )
       .then(function(result) {
         setRows(Object.values(result.data.eterUsers.edges))
-        console.log(Object.values(result.data.eterUsers.edges))
       })
-
-  },[])
+  },[ xsrf ])
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -260,7 +178,7 @@ export default function EnhancedTable() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.node.userLogin);
+      const newSelecteds = rows.map((n) => n.node.id);
       setSelected(newSelecteds);
       return;
     }
@@ -277,71 +195,62 @@ export default function EnhancedTable() {
     setPage(0);
   };
 
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  
-
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <EnhancedTableToolbar numSelected={selected.length} />
+        <EnhancedTableToolbar selected={selected} />
         <TableContainer>
           <Table
             className={classes.table}
             aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
+            size= 'medium'
             aria-label="enhanced table"
           >
             <EnhancedTableHead
-              classes={classes}
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
+              classes = { classes }
+              numSelected = { selected.length }
+              order = { order }
+              orderBy = { orderBy }
+              onSelectAllClick = { handleSelectAllClick }
+              onRequestSort = { handleRequestSort }
+              rowCount = { rows.length }
             />
             <TableBody>
                 {stableSort(rows, getComparator(order, orderBy))
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                   .map((row, index) => {
-                    console.log(row.userLogin + " " + index)
+                    //mise a jour longue
                     return (
                       <BackOfficeTableRow 
-                        row={row}
-                        index={index}
-                        selected = {selected}
-                        setSelected = {setSelected}
+                        row = { row }
+                        setRows = { setRows }
+                        index = { index }
+                        selected = { selected }
+                        setSelected = { setSelected }
                       />
                     )
                   })
                 }
                 {emptyRows > 0 && (
-                  <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                    <TableCell colSpan={6} />
+                  <TableRow style = { { height: 53 * emptyRows } }>
+                    <TableCell colSpan = { 6 } />
                   </TableRow>
                 )}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
+          rowsPerPageOptions = {[5, 10, 25, { label: 'All', value: -1 }]}
+          component = "div"
+          count = { rows.length }
+          rowsPerPage = { rowsPerPage }
+          page = { page }
+          onChangePage = { handleChangePage }
+          onChangeRowsPerPage = { handleChangeRowsPerPage }
         />
       </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
     </div>
   );
 }
